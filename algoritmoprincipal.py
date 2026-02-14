4ce2174e555aae53184c0018694ddf88c55005ed
Shioran <andresmars45@gmail.com>
2026-02-13 20:43:15 -0500

    algoritmos y ciclos

---
+++ algoritmoprincipal.py
@@ -0,0 +1,10 @@
+#Declarando variables
+
+id=8568
+nombres="Andres Felipe Torres"
+documento="125886837"
+correo="andres@gmail.com"
+telefono="3178522569"
+promedio=3.2
+esBecado=True
+semestre=3
---
+++ diccionarios.py
@@ -0,0 +1,28 @@
+#creando diccionarios en python
+estudianteUno={
+
+}
+
+#Poblando un diccionario / (llenandolo)
+
+estudianteUno["id"]=875
+estudianteUno["nombres"]="belmurd"
+estudianteUno["correo"]="belmunt@gmail.com"
+estudianteUno["identificacion"]="2458671"
+estudianteUno["promedio"]=3.6
+estudianteUno["esBecado"]=True
+estudianteUno["telefono"]="313524685"
+estudianteUno["semestre"]=3
+
+#Debo crear de forma dinamica los diccionarios
+
+#while
+'''contador=1
+while contador>10:
+    print("hola")
+    contador+=1
+print("adios")'''
+
+#for
+for i in range(10):
+    print(i)
